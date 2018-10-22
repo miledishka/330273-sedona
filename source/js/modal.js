dc1b5046782bb606d96d73868deb3970faa11c2d
Julija Rodionova <julija.rodionowa@yandex.ru>
2018-10-22 21:21:52 +0300

    правки по пиксель перфект переделан блок видео сделана модалка формы

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,61 +1,38 @@
-function toggle(id, toggled_class) {
-  element = document.getElementById(id);
+var form = document.querySelector("#review__form");
+var closeButtons = document.querySelectorAll(".modal__btn--close");
 
-  if (element.classList) {
-    element.classList.toggle(toggled_class);
+//
+// window.addEventListener("keydown", function (evt) {
+//   if (evt.keyCode === 27 && modal.classList.contains("active")) {
+//     toggleModal("modal");
+//   }
+// });
+
+var formSubmitHandler = function (evt) {
+  evt.preventDefault();
+
+  var modalSuccess = document.querySelector(".modal__success");
+  var modalError = document.querySelector(".modal__error");
+
+  var name = form.querySelector("input[name=name]");
+  var surname = form.querySelector("input[name=surname]");
+  var email = form.querySelector("input[name=email]");
+  var phone = form.querySelector("input[name=phone]");
+
+  modalSuccess.classList.remove("active");
+  modalError.classList.remove("active");
+
+  if (!name.value || !email.value || !phone.value || !surname.value) {
+    modalError.classList.add('active');
   } else {
-    var classes = element.className.split(" ");
-    var i = classes.indexOf(toggled_class);
-
-    if (i >= 0) {
-      classes.splice(i, 1);
-    } else {
-      classes.push(toggled_class);
-    }
-
-    element.className = classes.join(" ");
+    modalSuccess.classList.add('active');
   }
-}
+};
+var closeModal = function(evt) {
+  evt.target.closest(".modal").classList.remove("active");
+};
 
-function toggleModal(id) {
-  toggle(id, "active");
-  toggle("modal-overlay", "active");
-  modal = document.getElementById(id);
-  overlay = document.getElementById("modal-overlay");
-  removeError(modal, overlay);
-}
-
-function removeError(modal, overlay) {
-  if (!modal.classList.contains("active")) {
-    modal.classList.remove("modal__error");
-  }
-}
-
-var overlay = document.getElementById("modal-overlay");
-var modal = document.getElementById("modal");
-var form = modal.querySelector(".form-section");
-var name = form.querySelector("input[name=name]");
-var surname = form.querySelector("input[name=surname]");
-var email = form.querySelector("input[name=email]");
-var phone = form.querySelector("input[name=phone]");
-
-
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27 && modal.classList.contains("active")) {
-    toggleModal("modal");
-  }
-});
-
-form.addEventListener("submit", function (evt) {
-  if (!name.value || !email.value || !phone.value) {
-    evt.preventDefault();
-    modal.classList.remove("modal__error");
-    modal.offsetWidth = modal.offsetWidth;
-    modal.classList.add("modal__error");
-  } else {
-    modal.classList.remove("modal__success");
-    modal.offsetWidth = modal.offsetWidth;
-    modal.classList.add("modal__success");
-  }
+form.addEventListener("submit", formSubmitHandler);
+closeButtons.forEach(function(closeButton) {
+  closeButton.addEventListener("click", closeModal);
 });
